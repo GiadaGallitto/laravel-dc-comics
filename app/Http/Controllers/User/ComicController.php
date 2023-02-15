862f03e0c68de245dee8c68a7ca5d70b31be8d34
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-15 14:09:32 +0100

    Add fillables on store function of Controller

--- app/Http/Controllers/User/ComicController.php
+++ app/Http/Controllers/User/ComicController.php
@@ -41,14 +41,18 @@ class ComicController extends Controller
     {
         //
         $data = $request->all();
-        $newComic = new Comic();
-            $newComic->title = $data['title'];
-            $newComic->description = $data['description'];
-            $newComic->thumb = $data['thumb'];
-            $newComic->price = $data['price'];
-            $newComic->series = $data['series'];
-            $newComic->sale_date = $data['sale_date'];
-            $newComic->type = $data['type'];
+        // $newComic = new Comic();
+        //     $newComic->title = $data['title'];
+        //     $newComic->description = $data['description'];
+        //     $newComic->thumb = $data['thumb'];
+        //     $newComic->price = $data['price'];
+        //     $newComic->series = $data['series'];
+        //     $newComic->sale_date = $data['sale_date'];
+        //     $newComic->type = $data['type'];
+        //     $newComic->save();
+
+            $newComic = new Comic();
+            $newComic->fill($data);
             $newComic->save();
 
             return redirect()->route('comics.show', $newComic->id);
